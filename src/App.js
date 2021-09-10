3211d0c3ec8c97041fff32b5528d834bf1539f5f
James <james@terra.money>
2021-09-10 12:18:32 +0900

    add stringPlus

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { div, minus, multiple, plus } from "./module/src/scripts";
+import { div, minus, multiple, plus, stringPlus } from "./module/src/scripts";
 import s from "./test.module.scss";
 
 const App = () => {
@@ -7,6 +7,7 @@ const App = () => {
   const divValue = div(9, 3);
   const multipleValue = multiple(10, 2);
   const minusValue = minus(4, 3);
+  const stringValue = stringPlus("h", "i");
 
   return (
     <div className={s.front}>
@@ -14,6 +15,7 @@ const App = () => {
       <div>div:{divValue}</div>
       <div>multiple:{multipleValue}</div>
       <div>minus:{minusValue}</div>
+      <div>string:{stringValue}</div>
     </div>
   );
 };
